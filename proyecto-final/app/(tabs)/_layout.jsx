19a407ffc96182fd66ecece64756223e4484b933
AgustinPose <poseagus15@gmail.com>
2024-11-13 19:08:17 -0300

    postear

--- proyecto-final/app/(tabs)/_layout.jsx
+++ proyecto-final/app/(tabs)/_layout.jsx
@@ -32,6 +32,19 @@ export default function TabLayout() {
           ),
         }}
       />
+
+
+      <Tabs.Screen
+        name="postear"
+        options={{
+          title: 'Postear',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          ),
+        }}
+      />
+
+      
     </Tabs>
   );
 }
